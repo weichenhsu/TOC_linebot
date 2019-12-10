1e7a802bba8d5827ead85a56a9defb98fc27eaff
weichenhsu <weichen130688@gmail.com>
2019-12-10 12:16:28 +0800

    test new method

--- utils.py
+++ utils.py
@@ -28,21 +28,18 @@ def send_image(reply_token, text):
 
 def movie(reply_token):
     print("movie")
-    line_bot_api = LineBotApi(channel_access_token)
-
-    target_url = 'https://www.cwb.gov.tw/V8/C/W/County/County.html?CID=67'
+    target_url = 'https://movies.yahoo.com.tw/'
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')   
     content = ""
-    for index, data in enumerate(soup.select('tem-C is-active')):
+    for index, data in enumerate(soup.select('div.movielist_info h1 a')):
         if index == 20:
             return content       
         title = data.text
-        content += "{}\n".format(title)
-        #link =  data['href']
-        #content += '{}\n{}\n'.format(title, link)
+        link =  data['href']
+        content += '{}\n{}\n'.format(title, link)
 
     line_bot_api.reply_message(reply_token,TextSendMessage(text=content))
     return "OK"
